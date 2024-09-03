322340528f2080b058784ada59fd34869f6a088e
崎津 祥汰 <shota.sakitsu@virtlive.jp>
2024-09-03 22:32:17 +0900

    i18nモジュールを準備しました。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
-	modules: ["@nuxtjs/tailwindcss", "nuxt-aos", "@nuxt/image", '@bootstrap-vue-next/nuxt'],
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"nuxt-aos",
+		"@nuxt/image",
+		'@bootstrap-vue-next/nuxt',
+		"@nuxtjs/i18n",
+	],
 	ssr: false,
 	tailwindcss: {
 		config: {
