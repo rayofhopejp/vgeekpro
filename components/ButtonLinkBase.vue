9c8a653ff9544b794f95af27ea9d3a0ad163c352
崎津 祥汰 <shota.sakitsu@virtlive.jp>
2024-09-04 21:32:58 +0900

    変更途中にコミットしてしまいました。ボタン機能追加単独では壊れます

--- components/ButtonLinkBase.vue
+++ components/ButtonLinkBase.vue
@@ -10,7 +10,7 @@ const props = defineProps<ButtonLinkBaseAttributes>()
 </script>
 
 <template>
-	<a v-if="!(props.useSimpleAnchor ?? false)" :class="`tw-self-stretch tw-flex tw-shrink tw-items-center tw-rounded-full tw-bg-rose-500 hover:tw-bg-rose-700 tw-text-white tw-px-8 tw-py-4${(props.class !=)}`" :href="props.href" :rel="props.rel ?? ''" :target="props.target ?? ''">
+	<a v-if="!(props.useSimpleAnchor ?? false)" :class="`tw-self-stretch tw-flex tw-shrink tw-items-center tw-rounded-full tw-bg-rose-500 hover:tw-bg-rose-700 tw-text-white tw-px-8 tw-py-4${(props.class != undefined ? ` ${props.class}` : '')}`" :href="props.href" :rel="props.rel ?? ''" :target="props.target ?? ''">
 		<slot />
 	</a>
 	<a v-else :class="props.class ?? ''" :href="props.href" :rel="props.rel ?? ''" :target="props.target ?? ''">
