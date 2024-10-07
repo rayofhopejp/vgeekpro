b929704e6c4d931618fd4ea1c5f391b508549711
崎津 祥汰 <shota.sakitsu@virtlive.jp>
2024-10-07 09:16:37 +0900

    動画・配信一覧を最適化

--- components/VideoList.vue
+++ components/VideoList.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import type { Member } from "~/pages/index.vue";
 
 type VideoMemberAttributes = {
 	url: string,
@@ -20,7 +19,6 @@ type Video = {
 }
 
 const { locale } = useI18n();
-const localePath = useLocalePath();
 const config = useRuntimeConfig();
 
 const props = defineProps<VideoMemberAttributes>();
@@ -43,29 +41,77 @@ watch(() => props.url, async()=>{
 </script>
 
 <template>
-	<section class="tw-w-[80vw] tw-h-[300px] tw-my-3">
+	<section class="list-container tw-h-[400px] tw-my-3">
 		<div v-if="isStart === false && memberVideo.length !== 0" class="tw-leading-loose tw-text-black tw-flex tw-flex-row tw-h-full tw-overflow-x-auto scrollbar">
 			<div v-for="videoItem in memberVideo" class="tw-h-full tw-mx-2">
-				<a :key="videoItem.videoId" :href="videoItem.url"  class="tw-h-full">
-					<div class="flex flex-col tw-w-[300px] tw-h-[85%] tw-rounded-lg tw-bg-stone-100 tw-overflow-hidden tw-items-center tw-justify-center tw-shadow-md">
-						<img
-							:alt="videoItem.title"
-							:height="1280"
-							:src="videoItem.thumbnail"
-							:width="720"
-							class="tw-h-auto"
-						/>
-						<div class="p-2">
-							<p class="tw-text-sm tw-text-center textline-hidden">{{ `${videoItem.title}`}}</p>
-						</div>
-					</div>
+				<a :key="videoItem.videoId" :href="videoItem.url"  class="tw-h-full disable-link-icons">
+					<BCard class="tw-w-[320px] tw-h-full">
+						<template v-slot:img>
+							<BCardImg :alt="videoItem.title" :height="1280" :src="videoItem.thumbnail" :width="720" />
+						</template>
+						<BCardTitle>
+							<p class="tw-text-sm tw-text-center textline-hidden">{{ videoItem.title }}</p>
+						</BCardTitle>
+						<template v-slot:footer>
+							<div class="tw-h-[5em] description-text">
+								<small class="channel-name">{{ videoItem.channelTitle }}</small><br>
+								<small>
+									<span v-if="videoItem.liveBroadcast == 'upcoming'">
+										<span class="tw-me-1">{{ $t("videoListBox.scheduledAtHead") }}</span>
+										<span>{{ new Date(videoItem.liveScheduledStartTime).toLocaleString(locale, {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"}) }}</span>
+										<span class="tw-ms-1">{{ $t("videoListBox.scheduledAtFoot") }}</span>
+									</span>
+									<span v-else-if="videoItem.liveBroadcast == 'live'">
+										<span class="tw-me-1">{{ $t("videoListBox.fromHead") }}</span>
+										<span>{{ new Date(videoItem.liveActualStartTime).toLocaleString(locale, {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"}) }}</span>
+										<span class="tw-ms-1">{{ $t("videoListBox.fromFoot") }}</span>
+									</span>
+									<span v-else>
+										<span v-if="videoItem.liveActualStartTime != ''">
+											<span class="tw-me-1">{{ $t("videoListBox.fromHead") }}</span>
+											<span>{{ new Date(videoItem.liveActualStartTime).toLocaleString(locale, {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"}) }}</span><br>
+											<span class="tw-ms-1">{{ $t("videoListBox.fromFoot") }}</span>
+											<span class="tw-me-1">{{ $t("videoListBox.toHead") }}</span>
+											<span>{{ new Date(videoItem.liveActualEndTime).toLocaleString(locale, {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"}) }}</span>
+											<span class="tw-ms-1">{{ $t("videoListBox.toFoot") }}</span>
+										</span>
+										<span v-else>
+											<span class="tw-me-1">{{ $t("videoListBox.postedAtHead") }}</span>
+											<span>{{ new Date(videoItem.publishedAt).toLocaleString(locale, {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"}) }}</span>
+											<span class="tw-ms-1">{{ $t("videoListBox.postedAtFoot") }}</span>
+										</span>
+									</span>
+								</small>
+							</div>
+						</template>
+					</BCard>
 				</a>
 			</div>
 		</div>
-		<div v-if="isStart === false && memberVideo.length === 0" class="tw-leading-loose tw-flex tw-flex-row tw-h-full tw-flex tw-items-center tw-justify-center ">
+		<div v-if="isStart === false && memberVideo.length === 0" class="tw-leading-loose tw-flex tw-flex-row tw-h-full tw-items-center tw-justify-center ">
 			<p class="tw-text-5xl">Not Found</p>
 		</div>
 	</section>
 </template>
 
-<style scoped></style>
+<style lang="less" scoped>
+.list-container {
+	width: calc(100vw - calc(8px + 60px + 1.5rem + 5rem))
+}
+
+.description-text {
+	font-size: 10.5pt;
+	line-height: 10.5pt;
+}
+
+.channel-name {
+	font-size: 12pt;
+}
+
+@media (min-width: 1280px) {
+	.list-container {
+		width: calc(100vw - calc(8px + 60px + 1.5rem + 7rem))
+	}
+}
+
+</style>
--- components/VideoListBox.vue
+++ components/VideoListBox.vue
@@ -1,43 +1,63 @@
 <script lang="ts" setup>
-import type { Member } from "~/pages/index.vue";
+import type {Member} from "~/pages/index.vue";
 
 type VideoMemberAttributes = {
 	members: Member[],
 }
 
+type VideoTab = {
+	label: string,
+	type: string,
+	url: string
+}
 
-const { locale } = useI18n();
+const {locale} = useI18n();
 const localePath = useLocalePath();
 const config = useRuntimeConfig();
 
 const props = defineProps<VideoMemberAttributes>();
 
-const tabs = ref<{label:string,url:string}[]>(reload());
+const tabs = ref<VideoTab[]>(reload());
 
-const selectTab = ref<{label:string,url:string}>(tabs.value[0]);
+const selectTab = ref<VideoTab>(tabs.value[0]);
 
-function tabChange(tab: {label:string,url:string}){
+function tabChange(tab: VideoTab) {
 	selectTab.value = tab;
 }
 
-function reload(){
+function reload() {
 	return [
-		{label: "ライブ前" , url: `${config.public.WEB_API}/api/search?name=${props.members.map(e=>e.profileId).join(",")}&type=liveBefore`},
-		{label: "ライブ" , url: `${config.public.WEB_API}/api/search?name=${props.members.map(e=>e.profileId).join(",")}&type=liveNow`},
-		{label: "ライブ後" , url: `${config.public.WEB_API}/api/search?name=${props.members.map(e=>e.profileId).join(",")}&type=liveAfter`},
-		{label: "ビデオ" , url: `${config.public.WEB_API}/api/search?name=${props.members.map(e=>e.profileId).join(",")}&type=video`},
+		{
+			label: "videoListBox.scheduled",
+			type: "schedule",
+			url: `${config.public.WEB_API}/api/search?name=${props.members.map(e => e.profileId).join(",")}&type=liveBefore`},
+		{
+			label: "videoListBox.live",
+			type: "live",
+			url: `${config.public.WEB_API}/api/search?name=${props.members.map(e => e.profileId).join(",")}&type=liveNow`
+		},
+		{
+			label: "videoListBox.archive",
+			type: "archive",
+			url: `${config.public.WEB_API}/api/search?name=${props.members.map(e => e.profileId).join(",")}&type=liveAfter`
+		},
+		{
+			label: "videoListBox.video",
+			type: "video",
+			url: `${config.public.WEB_API}/api/search?name=${props.members.map(e => e.profileId).join(",")}&type=video`
+		},
 	];
 }
 
-watch(() => props.members, async()=>{
+watch(() => props.members, async () => {
 	tabs.value = reload();
-	const newTab = tabs.value.find(e=>e.label === selectTab.value.label);
-	if(newTab){
+	const newTab = tabs.value.find(e => e.type === selectTab.value.type);
+	if (newTab) {
 		selectTab.value = newTab;
-	}else{
+	} else {
 		selectTab.value = tabs.value[0];
 	}
-},{
+}, {
 	immediate: true
 });
 
@@ -47,12 +67,12 @@ watch(() => props.members, async()=>{
 <template>
 	<div class="tw-flex tw-flex-row tw-text-slate-800">
 		<button v-for="tab in tabs" class="tw-rounded-t-lg tw-py-3 tw-px-5 tw-border-4 tw-border-b-0 tw-border-t-[15px]" v-on:click="()=>tabChange(tab)"
-			:class="{'tw-border-[#F43F5E]': tab.label === selectTab.label}">
-			{{`${tab.label}`}}
+				:class="{'tw-border-[#F43F5E]': tab.type === selectTab.type}">
+			{{ $t(tab.label) }}
 		</button>
 	</div>
 	<div class="tw-border-4 tw-border-[#F43F5E] tw-p-3 tw-rounded-b-lg tw-rounded-tr-lg tw-text-slate-800/30">
-		<VideoList :members="props.members" :url="selectTab.url" />
+		<VideoList :members="props.members" :url="selectTab.url"/>
 	</div>
 </template>
 
