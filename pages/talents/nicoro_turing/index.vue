4f411a42d4df71b5c623bd77b095cbf6dcea8e0d
崎津 祥汰 <shota.sakitsu@virtlive.jp>
2024-12-24 10:37:01 +0900

    ひょんぢゅ氏の名前変更を反映しました。その他の反映は年始頃の予定です。

--- pages/talents/nicoro_turing/index.vue
+++ pages/talents/nicoro_turing/index.vue
@@ -2,10 +2,10 @@
 definePageMeta({
 	layout: 'member-page'
 })
-const name = "ひょんぢゅ";
-const yomi = "Hyondyu";
+const name = "にころ・ちゅーりんぐ";
+const yomi = "Nicoro Turing";
 const description = "コンパイラが通らない！デバッグ神社を立てろ！";
-const imageId = "hyondyu";
+const imageId = "nicoro_turing";
 useSeoMeta({
 	title: name,
 	description,
@@ -29,7 +29,7 @@ useSeoMeta({
 	<section class="tw-px-10 xl:tw-px-14 tw-py-14 tw-text-base">
 		<section>
 			<ProfileHead :catchphrase="description" :name="name" :yomi="yomi">
-				こんにちは、皆さん！Pythonを主要プログラミング言語として活動しているIT系YouTuberのひょんぢゅです。私のチャンネルでは、Pythonを使ったプログラミングのチュートリアルやプロジェクト、最新のITトピックをわかりやすくお届けしています。<br/>
+				こんにちは、皆さん！Pythonを主要プログラミング言語として活動しているIT系YouTuberのにころ・ちゅーりんぐです。私のチャンネルでは、Pythonを使ったプログラミングのチュートリアルやプロジェクト、最新のITトピックをわかりやすくお届けしています。<br/>
 				私の好きなことは、ゲームとタロットカード、そしてTRPGです。プログラミングの合間には、ゲームでリフレッシュしたり、タロットカードで未来を占ったり、TRPGで友達と冒険に出たりしています。これらの趣味を通じて、皆さんにも楽しさと新しい発見をシェアできればと思っています。<br/>
 				プログラミングの技術だけでなく、ゲームやタロット、TRPGの話題もお楽しみください！チャンネル登録して、一緒に楽しい時間を過ごしましょう。質問やリクエストがあれば、コメントで気軽に教えてくださいね！<br/>
 			</ProfileHead>
